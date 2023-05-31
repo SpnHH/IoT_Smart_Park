51dd56afa773841e8badcadd23eabd4003981843
DESKTOP-BHD83GF\Traian <traiancalcan2@gmail.com>
2023-05-31 09:39:01 +0300

    Initial commit

---
+++ src/main/java/com/example/Iot_Smart_Park/IotSmartParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Iot_Smart_Park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSmartParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSmartParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Iot_Smart_Park/IotSmartParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Iot_Smart_Park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotSmartParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
